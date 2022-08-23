fa9046ca4f04a4e6560bbd6ef80b04173f9f61a7
John Skoubourdis <scoumbourdisj@gmail.com>
2022-08-23 07:18:05 +0300

    first commit 🚀🚀🚀

---
+++ translate.py
@@ -0,0 +1,38 @@
+import json
+import os
+
+# Everything is based on the english.json file
+# For example if there is a key missing in English then we are also removing it from all the other languages
+# If we have added a new key then we are adding it to all the other languages as well
+englishFile = open('json/English.json')
+englishData = json.load(englishFile)
+englishFile.close()
+
+def jsonToPhp(jsonData, language):
+    phpFile = open('php/' + language + '.php', 'w')
+    phpFile.write('<?php\n\n')
+    phpFile.write('return [\n')
+    # sort by key jsonData
+    for key in sorted(jsonData):
+        phpFile.write('    \'' + key + '\' => \'' + jsonData[key] + '\',\n')
+    phpFile.write('];\n')
+    phpFile.close()
+
+jsonToPhp(englishData, 'English')
+
+# Read all the folder within the folder json with extension .json
+for filename in os.listdir('json'):
+    if filename.endswith('.json'):
+        # Open the file and read the englishData
+        file = open(f'json/{filename}')
+        languageData = json.load(file)
+
+        # Loop through the englishData and check if the key is missing in the languageData
+        for key, value in englishData.items():
+            if key not in languageData:
+                languageData[key] = value
+
+        file.close()
+
+        # Write the languageData to the filename and remove the filename extension
+        jsonToPhp(languageData, filename.replace('.json', ''))
